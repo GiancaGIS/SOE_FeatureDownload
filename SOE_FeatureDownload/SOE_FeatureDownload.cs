7dd5c74aea6b14074cb15a664ad5bf55a7e44fed
Giancarlo Messina <giancarlomex@outlook.com>
2020-06-10 11:27:54 +0200

    Committato client html

---
+++ SOE_FeatureDownload/SOE_FeatureDownload.cs
@@ -0,0 +1,203 @@
+﻿// Copyright 2018 ESRI
+// 
+// All rights reserved under the copyright laws of the United States
+// and applicable international laws, treaties, and conventions.
+// 
+// You may freely redistribute and use this sample code, with or
+// without modification, provided you include the original copyright
+// notice and use restrictions.
+// 
+// See the use restrictions at <your Enterprise SDK install location>/userestrictions.txt.
+// 
+
+using ESRI.ArcGIS.DataSourcesGDB;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Server;
+using ESRI.ArcGIS.SOESupport;
+using SOE_Utilita;
+using System;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Text;
+
+//TODO: sign the project (project properties > signing tab > sign the assembly)
+//      this is strongly suggested if the dll will be registered using regasm.exe <your>.dll /codebase
+
+
+namespace SOE_FeatureDownload
+{
+    [ComVisible(true)]
+    [Guid("d186f6b3-2bec-4b59-8110-5dfad87bca63")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ServerObjectExtension("MapServer",//use "MapServer" if SOE extends a Map service and "ImageServer" if it extends an Image service.
+        AllCapabilities = "",
+        DefaultCapabilities = "",
+        Description = "",
+        DisplayName = "SOE_FeatureDownload",
+        Properties = "",
+        SupportsREST = true,
+        SupportsSOAP = false)]
+    public class SOE_FeatureDownload : IServerObjectExtension, IObjectConstruct, IRESTRequestHandler
+    {
+        private string soe_name;
+
+        private IPropertySet configProps;
+        private IServerObjectHelper serverObjectHelper;
+        private ServerLogger logger;
+        private IRESTRequestHandler reqHandler;
+
+        public SOE_FeatureDownload()
+        {
+            soe_name = this.GetType().Name;
+            logger = new ServerLogger();
+            reqHandler = new SoeRestImpl(soe_name, CreateRestSchema()) as IRESTRequestHandler;
+        }
+
+        #region IServerObjectExtension Members
+
+        public void Init(IServerObjectHelper pSOH)
+        {
+            serverObjectHelper = pSOH;
+        }
+
+        public void Shutdown()
+        {
+        }
+
+        #endregion
+
+        #region IObjectConstruct Members
+
+        public void Construct(IPropertySet props)
+        {
+            configProps = props;
+        }
+
+        #endregion
+
+        #region IRESTRequestHandler Members
+
+        public string GetSchema()
+        {
+            return reqHandler.GetSchema();
+        }
+
+        public byte[] HandleRESTRequest(string Capabilities, string resourceName, string operationName, string operationInput, string outputFormat, string requestProperties, out string responseProperties)
+        {
+            return reqHandler.HandleRESTRequest(Capabilities, resourceName, operationName, operationInput, outputFormat, requestProperties, out responseProperties);
+        }
+
+        #endregion
+
+        private RestResource CreateRestSchema()
+        {
+            RestResource rootRes = new RestResource(soe_name, false, RootResHandler);
+
+            RestOperation operazioneRestDownloadFeature = new RestOperation("oggettiDownload",
+                                                      new string[] { "listaOID", "URLServiceLayer" },
+                                                      new string[] { "json" },
+                                                      OperazioneRestDownloadFeatureHandler);
+
+            rootRes.operations.Add(operazioneRestDownloadFeature);
+
+            return rootRes;
+        }
+
+        private byte[] RootResHandler(NameValueCollection boundVariables, string outputFormat, string requestProperties, out string responseProperties)
+        {
+            responseProperties = null;
+
+            JsonObject result = new JsonObject();
+
+            return Encoding.UTF8.GetBytes(result.ToJson());
+        }
+
+        private byte[] OperazioneRestDownloadFeatureHandler(NameValueCollection boundVariables,
+                                                  JsonObject operationInput,
+                                                      string outputFormat,
+                                                      string requestProperties,
+                                                  out string responseProperties)
+        {
+            responseProperties = null;
+
+            #region Istanzio il JSON Result
+            JsonObject result = new JsonObject();
+            result.AddBoolean("hasError", false);
+            #endregion
+
+            bool found = operationInput.TryGetArray("listaOID", out object[] paramListaOID);
+            if (!found || paramListaOID == null)
+                throw new ArgumentNullException("listaOID");
+
+            bool okParam2 = operationInput.TryGetString("URLServiceLayer", out string paramURL);
+            if (!okParam2 || paramURL == String.Empty)
+                throw new ArgumentNullException("URLServiceLayer");
+
+            try
+            {
+                result.AddArray("interno", paramListaOID);
+
+                // Ricavo Feature Class dietro al Service Layer
+                IFeatureClass featureClass = MapServiceHelper_GiancaGIS.RicavaFCDaURLServiceLayer(this.serverObjectHelper, paramURL);
+
+                IWorkspace workspace = WorkspaceHelper_GiancaGIS.RicavaWorkSpaceFC(featureClass, out IPropertySet propertySetIN, 
+                    out string tipoWorkspace, out bool errore, out string msgErrore);
+
+                this.CreaWorkSpaceOutput(out IPropertySet propertySetOUT, out string pathFGDB);
+
+                SOE_Utilita.WorkspaceHelper_GiancaGIS.CopiaFeatureClass(propertySetIN, propertySetOUT, (featureClass as IDataset).Name, out string warning);
+            
+            }
+            catch (Exception errore)
+            {
+                result.AddString("errorDescription", errore.Message);
+                result.AddBoolean("hasError", true);
+            }
+
+            return Encoding.UTF8.GetBytes(result.ToJson());
+        }
+
+        private void CreaWorkSpaceOutput(out IPropertySet propertySet, out string pathFGDB)
+        {
+            IWorkspaceFactory2 workF = new FileGDBWorkspaceFactory() as IWorkspaceFactory2;
+            Guid g = Guid.NewGuid();
+
+            string timeStamp = DateTime.UtcNow.ToString("dd-MM-yyyy", CultureInfo.CurrentUICulture);
+
+            string basePath = $@"C:\arcgisserver\directories\arcgisoutput\_ags_{g}";
+
+            string nomeFGDB = $@"FGDB_{timeStamp}.gdb";
+
+            pathFGDB = System.IO.Path.Combine(basePath, nomeFGDB);
+
+            if (!System.IO.Directory.Exists(pathFGDB))
+                System.IO.Directory.CreateDirectory(pathFGDB);
+
+            #region Parte dedicata alla creazione di un File Geodatabase
+            // Istanzio l'oggetto singleton
+            IWorkspaceFactory2 workspaceFactory = new FileGDBWorkspaceFactory() as IWorkspaceFactory2;
+               
+            workspaceFactory.Create(pathFGDB, nomeFGDB, null, 0);
+
+            #endregion
+
+            // Mi dedico nella creazione del FGDB locale
+            propertySet = new PropertySetClass();
+            propertySet.SetProperty("DATABASE", pathFGDB);
+
+            #region Rilascio tutti gli oggetti singleton
+            int resLeft = 0;
+            do
+            {
+                resLeft = System.Runtime.InteropServices.Marshal.ReleaseComObject(workspaceFactory);
+            }
+            while (resLeft > 0);
+            #endregion
+
+        }
+
+    }
+}
---
+++ SOE_FeatureDownload/helper.cs
@@ -0,0 +1,327 @@
+﻿using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.DataSourcesGDB;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Server;
+using ESRI.ArcGIS.SOESupport;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace SOE_Utilita
+{
+    /// <summary>
+    /// class of Helper
+    /// </summary>
+    internal class GeneralHelper_GiancaGIS
+    {
+        /// <summary>
+        /// Converte una Feature Class, opportunamente filtrata in un oggetto RecordSet
+        /// </summary>
+        /// <param name="featureClass">feature class input</param>
+        /// <param name="queryFilter">query filter</param>
+        /// <returns>return Recordset</returns>
+        internal static IRecordSet2 ConvertToRecordset(IFeatureClass featureClass, IQueryFilter2 queryFilter)
+        {
+            IRecordSet recordSet = new RecordSetClass();
+            IRecordSetInit recordSetInit = recordSet as IRecordSetInit;
+            recordSetInit.SetSourceTable(featureClass as ITable, queryFilter);
+
+            return (IRecordSet2)recordSetInit;
+        }
+
+        /// <summary>
+        /// Converte una Feature Class, opportunamente filtrata in un oggetto RecordSet
+        /// </summary>
+        /// <param name="featureClass"></param>
+        /// <param name="spatialFilter"></param>
+        /// <returns></returns>
+        internal static IRecordSet2 ConvertToRecordset(IFeatureClass featureClass, ISpatialFilter spatialFilter)
+        {
+            IRecordSet recordSet = new RecordSetClass();
+            IRecordSetInit recordSetInit = recordSet as IRecordSetInit;
+            recordSetInit.SetSourceTable(featureClass as ITable, spatialFilter);
+
+            return (IRecordSet2)recordSetInit;
+        }
+
+        /// <summary>
+        /// Converte una lista di IGeometry in un array di JsonObjects
+        /// </summary>
+        /// <param name="geometries">list of IGeometry</param>
+        /// <returns>array of JsonObject</returns>
+        internal static object[] GetListJsonObjects(List<IGeometry> geometries)
+        {
+            List<JsonObject> jsonObjects = new List<JsonObject>();
+            geometries.ForEach(g =>
+            {
+                jsonObjects.Add(Conversion.ToJsonObject(g));
+            });
+
+            return jsonObjects.ToArray();
+        }
+    }
+
+    /// <summary>
+    /// Classe contenente metodi utili per i Map Service
+    /// </summary>
+    internal class MapServiceHelper_GiancaGIS : GeneralHelper_GiancaGIS
+    {
+        /// <summary>
+        /// Metodo che restituisce una Feature Class dato un Server Object e il Url del Service Layer
+        /// </summary>
+        /// <param name="serverObjectHelper">Server Object</param>
+        /// <param name="URLServiceLayer">URL Service Layer</param>
+        /// <returns></returns>
+        internal static IFeatureClass RicavaFCDaURLServiceLayer(IServerObjectHelper serverObjectHelper, string URLServiceLayer)
+        {
+            IFeatureClass fc;
+            try
+            {
+                // Ricavo il numero posizionale del Service Layer
+                int numPosizionale =  Convert.ToInt32(System.IO.Path.GetFileName(URLServiceLayer));
+
+                IMapServer4 mapServer = (IMapServer4)serverObjectHelper.ServerObject;
+                string nomeMapService = mapServer.DefaultMapName;
+
+                // Use IMapServerDataAccess to get the data
+                IMapServerDataAccess dataAccess = (IMapServerDataAccess)mapServer;
+
+                // Accedo alla Feature Class sorgente
+                fc = (IFeatureClass)dataAccess.GetDataSource(nomeMapService, numPosizionale);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+
+            return fc;
+        }
+    }
+
+    internal class WorkspaceHelper_GiancaGIS : MapServiceHelper_GiancaGIS
+    {
+        internal static IWorkspace RicavaWorkSpaceFC(IFeatureClass featureClass,
+            out IPropertySet propertySet, out string tipoWorkspace, out bool errore, out string msgErrore)
+        {
+            IWorkspace workspace = null;
+            errore = false;
+            msgErrore = String.Empty;
+            tipoWorkspace = String.Empty;
+            propertySet = null;
+
+            try
+            {
+                workspace = featureClass.FeatureDataset.Workspace;
+                tipoWorkspace = workspace.WorkspaceFactory.WorkspaceDescription[false];
+                propertySet = workspace.ConnectionProperties;
+            }
+            catch (Exception ex)
+            {
+                errore = true;
+                msgErrore = ex.Message;
+            }
+
+            return workspace;
+        }
+
+        internal static void CopiaFeatureClass
+            (IPropertySet propertySetIN, IPropertySet propertySetOUT,
+            string nomeFCIN,
+            out string warningErroriCopiaDati,
+            string WorkspaceFactoryProgID_IN = "esriDataSourcesGDB.SDEWorkspaceFactory",
+            string WorkspaceFactoryProgID_OUT = "esriDataSourcesGDB.FileGDBWorkspaceFactor")
+        {
+            try
+            {
+                warningErroriCopiaDati = String.Empty;
+
+                // Imposto la Workspace di output (il FGDB)
+                IWorkspaceName outWorkspaceName = new WorkspaceName() as IWorkspaceName;
+                outWorkspaceName.ConnectionProperties = propertySetOUT;
+                //outWorkspaceName.WorkspaceFactoryProgID = WorkspaceFactoryProgID_OUT;
+
+                IFeatureClassName OutFCName = new FeatureClassNameClass();
+
+                IDatasetName outDatasetName = OutFCName as IDatasetName;
+                outDatasetName.Name = nomeFCIN;
+                outDatasetName.WorkspaceName = outWorkspaceName;
+                IFeatureDatasetName outFeatureDatasetName = new FeatureDatasetNameClass();
+
+
+                // Imposto la Workspace di input
+                IWorkspaceFactory2 InworkF = new FileGDBWorkspaceFactory() as IWorkspaceFactory2;
+                IWorkspace InWorkspace = InworkF.Open(propertySetIN, 0);
+                IFeatureWorkspace featureWorkspace = InWorkspace as IFeatureWorkspace;
+                
+                IWorkspaceName inWorkspaceName = new WorkspaceName() as IWorkspaceName;
+                inWorkspaceName.ConnectionProperties = InWorkspace.ConnectionProperties;
+                //inWorkspaceName.WorkspaceFactoryProgID = WorkspaceFactoryProgID_IN;
+
+                IFeatureClassName InFCName = new FeatureClassNameClass();
+
+                IDatasetName InDatasetName = InFCName as IDatasetName;
+                InDatasetName.Name = nomeFCIN;
+                InDatasetName.WorkspaceName = inWorkspaceName;
+
+
+                // Mi dedico alla apertura della Feature Class di input per ricavare le field definitions
+                IFeatureClass featureClassIn = featureWorkspace.OpenFeatureClass(nomeFCIN);
+
+                // Valido gli attributi della Feature Class di Input
+                IFields fieldsIn = featureClassIn.Fields;
+                IFieldChecker fieldChecker = new FieldCheckerClass();
+                fieldChecker.Validate(fieldsIn, out IEnumFieldError enumFieldError, out IFields fieldsOut);
+
+                // Loop attraverso gli attributi per cercare quello geometrico
+                IField attributoGeometrico = null;
+                for (int cont = 0; cont < fieldsOut.FieldCount; cont ++)
+                {
+                    if (fieldsOut.Field[cont].Type == esriFieldType.esriFieldTypeGeometry)
+                    {
+                        attributoGeometrico = fieldsOut.Field[cont];
+                        break;
+                    }
+                }
+
+                // Ricavo la geometry definition della Feature Class di input
+                IGeometryDef geometryDefOUT_Fc = attributoGeometrico.GeometryDef;
+
+                // Per la Feature Class di output, fornisco la geometry definition di output, lo spatial index grid count
+                // e la grid size
+                IGeometryDefEdit geometryDefEdit = geometryDefOUT_Fc as IGeometryDefEdit;
+                geometryDefEdit.GridCount_2 = 1;
+                geometryDefEdit.GridSize_2[0] = CalcolaDefaultIndexFC(featureClassIn, out bool errore, out string msgErrore);
+
+                if (!errore)
+                {
+                    geometryDefEdit.SpatialReference_2 = ((IGeoDataset)featureClassIn).SpatialReference;
+
+                    // Mi preparo alla copia della Feature Class
+                    IFeatureDataConverter2 featureDataConverter = new FeatureDataConverterClass();
+                    IEnumInvalidObject enumInvalid = 
+                        featureDataConverter.ConvertFeatureClass(InDatasetName, null, null, outFeatureDatasetName, OutFCName, geometryDefOUT_Fc, fieldsOut, "", 1000, 0);
+
+                    IInvalidObjectInfo invalidObject = enumInvalid.Next();
+                    if (invalidObject != null)
+                        warningErroriCopiaDati = "Feature Class copiata ma con errori!";
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        internal static double CalcolaDefaultIndexFC(IFeatureClass featureClass, out bool errore, out string messaggioErrore)
+        {
+            double DefaultIndexGrid = -9999;
+            errore = false;
+            messaggioErrore = String.Empty;
+            const int fattore = 1;
+            int proporzionalita = 1;
+
+            double dblMaxDelta = 0;
+            double dblMinDelta = 1000000000000d;
+            double dblQuadratezzaExtent = 1;
+
+            try
+            {
+                int numeroFeatures = featureClass.FeatureCount(null) - 1;
+                if (numeroFeatures > 1000)
+                    proporzionalita = 1000;
+
+                if (numeroFeatures <= 0)
+                {
+                    DefaultIndexGrid = 1000;
+                    return DefaultIndexGrid;
+                }
+                else
+                {
+                    if(featureClass.ShapeType == esriGeometryType.esriGeometryPoint || featureClass.ShapeType == esriGeometryType.esriGeometryMultipoint)
+                    {
+                        DefaultIndexGrid = CalcolaDefaultIndex_Fc_Point(featureClass);
+                        return DefaultIndexGrid;
+                    }
+                    else
+                    {
+                        // Ricavo nome OID della Feature Class di input
+                        string OID = featureClass.OIDFieldName;
+
+                        // Ricavo Envelope della intera Feature Class
+                        IGeoDataset geoDataset = featureClass as IGeoDataset;
+                        IEnvelope envelope = geoDataset.Extent;
+
+                        // Ricavo una stima sulla "quadratezza" dell'extent della Feature Class
+
+                        dblMaxDelta = Math.Max(dblMaxDelta, Math.Max(envelope.Width, envelope.Height));
+                        dblMinDelta = Math.Min(dblMinDelta, Math.Min(envelope.Width, envelope.Height));
+
+                        if (dblMinDelta != 0)
+                        {
+                            dblQuadratezzaExtent =
+                                1 + (Math.Min(envelope.Width, envelope.Height) / Math.Max(envelope.Width, envelope.Height));
+                        }
+                        else
+                        {
+                            dblQuadratezzaExtent = dblQuadratezzaExtent + 0.0001d;
+                        }
+
+                        if (dblQuadratezzaExtent / proporzionalita > 0.5)
+                        {
+                            DefaultIndexGrid = (dblMinDelta + ((dblMaxDelta - dblMinDelta) / 2)) * fattore;
+                            return DefaultIndexGrid;
+                        }
+                        else
+                        {
+                            DefaultIndexGrid = Math.Round((dblMaxDelta / 2) * fattore, 3);
+                            return DefaultIndexGrid;
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                errore = true;
+                messaggioErrore = ex.Message;
+            }
+
+            return DefaultIndexGrid;
+        }
+
+        private static double CalcolaDefaultIndex_Fc_Point(IFeatureClass featureClass)
+        {
+            try
+            {
+                // Ricavo Envelope della intera Feature Class
+                IGeoDataset geoDataset = featureClass as IGeoDataset;
+                IEnvelope envelope = geoDataset.Extent;
+
+                // Calcolo la approssimata Primo Spatial Index
+                int numeroFeatures = featureClass.FeatureCount(null) - 1;
+
+                double DefaultIndexGridPoint;
+                if (numeroFeatures == 0 || envelope.IsEmpty)
+                {
+                    DefaultIndexGridPoint = 1000;
+                    return DefaultIndexGridPoint;
+                }
+                else
+                {
+                    double area = envelope.Height / envelope.Width;
+                    // Approssimo l'indice spaziale come la radice quadrata dell'area fratto il numero di features
+                    DefaultIndexGridPoint = Math.Sqrt(area / numeroFeatures);
+                    return DefaultIndexGridPoint;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
